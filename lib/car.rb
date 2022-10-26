49848e6b0bc38dbd89ed69e3ce88bcb4658f1eed
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-10-26 15:21:40 +0300

    done

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,10 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+    #overwrite the inherited #go method with one specific to Car
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+end
